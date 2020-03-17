e38c7aa5f5a3f3ee962847f0429df381daa53ca5
Guy Badman <guyagbadman@hotmail.com>
2020-03-17 14:28:12 +0000

    Updating to cope with region and for HACS integration

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # octocost
-Cost and Usage of Octopus Energy Agile Tariff
+Octocost is an app which works under [AppDaemon](https://www.home-assistant.io/docs/ecosystem/appdaemon/) within [Home Assistant](https://www.home-assistant.io/) which shows the yearly and month cost and usage of the Octopus Energy Agile Octopus Tariff
+It creates and sets sensors for yearly and monthly cost (£) and usage (kWh), up to yesterday:
+```
+sensor.octopus_yearly_cost
+sensor.octopus_yearly_usage
+sensor.octopus_monthly_cost
+sensor.octopus_monthly_usage
+```
+The data is updated once every two hours, although in reality the data Octopus Energy gets only seems to be updated once a day, so this is a compromise between trying to be up-to-date, and not hammering their servers, when the data doesn't update very frequently anyway.
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg)](https://github.com/custom-components/hacs)
+
+### Installation
+
+Use [HACS](https://github.com/custom-components/hacs) or download the octoblock directory from inside the apps directory [here](https://github.com/badguy99/octocost/releases) to your local apps directory, then add the configuration to enable the octocost module.
+
+
+### Apps.yaml Configuration
+```yaml
+octocost:
+  module: octocost 
+  class: OctoCost 
+  region: H
+  mpan: <13 digit MPAN number>
+  serial:  <Serial number>
+  auth: <Octopus Energy API Key>
+  startdate: 2020-02-23
+
+``` 
+
+The module and class sections need to remain as above, other sections should be changed as required.
+
+| Field     | Changeable | Example         |
+| -----     | ---------- | -------         |
+| Title     | Yes        | octocost        |
+| module    | No         | octocost        |
+| class     | No         | OctoCost        |
+| region    | Yes        | H               |
+| mpan      | Yes        | 2000012345678   |
+| serial    | Yes        | 20L123456       |
+| auth      | Yes        | sk_live_abcdefg |
+| startdate | Yes        | 2020-02-23      |
+
+ The `startdate` setting should be set to the date you started on the Agile Octopus tariff, not the date you joined Octopus Energy. It is used to adjust the start point if you joined within the current year or month, but should not be left blank if you joined earlier.
+
--- octocost.py
+++ octocost.py
@@ -1,60 +1,81 @@
+import appdaemon.plugins.hass.hassapi as hass
 import datetime
 import requests
 import json
 
-MPAN=!secret elec_mpan
-SERIAL=!secret elec_serial
 
+class OctoCost(hass.Hass):
+    def initialize(self):
+        self.auth = self.args['auth']
+        MPAN = self.args['mpan']
+        SERIAL = self.args['serial']
+		region = self.args['region']
+        self.startdate = datetime.date.fromisoformat(str(self.args['startdate']))
+        self.consumptionurl = 'https://api.octopus.energy/v1/electricity-meter-points/' + str(MPAN) + '/meters/' + str(SERIAL) + '/consumption/'
+        self.costurl= 'https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-' + str(region) + '/standard-unit-rates/'
+        time = datetime.datetime.now()
+        time = time + datetime.timedelta(seconds=5)
+        self.run_every(self.cost_and_usage_callback, time, 120 * 60)
 
-consumptionurl = 'https://api.octopus.energy/v1/electricity-meter-points/' + MPAN + '/meters/' + SERIAL + '/consumption/'
-costurl= 'https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-H/standard-unit-rates/'
+    def cost_and_usage_callback(self, kwargs):
+        today = datetime.date.today()
+        self.yesterday = datetime.date(today.year, today.month, today.day-1)
+        startyear = datetime.date(today.year, 1, 1)
+        startmonth = datetime.date(today.year, today.month, 1)
 
-auth=!secret octopus_api
+        if self.startdate > startmonth:
+            startmonth = self.startdate
 
-today = datetime.date.today()
-yesterday = datetime.date(today.year, today.month, today.day-1)
-#startyear = datetime.date(today.year, 1, 1)
-startyear = datetime.date(today.year, 2, 19)
-#startyear = datetime.date(today.year, 3, 1)
-numberdays = yesterday-startyear
-numberdays = numberdays.days
+        if self.startdate > startyear:
+            startyear = self.startdate
 
-expectedcount= ((numberdays+1)*48)-1
-expectedcount
+        monthlyusage, monthlycost = self.calculate_cost_and_usage(start=startmonth)
+        print('Total monthly usage: {} kWh'.format(monthlyusage))
+        print('Total monthly cost: {} p'.format(monthlycost))
 
+        yearlyusage, yearlycost = self.calculate_cost_and_usage(start=startyear)
+        print('Total yearly usage: {} kWh'.format(yearlyusage))
+        print('Total yearly cost: {} p'.format(yearlycost))
 
-rconsumption = requests.get(url=consumptionurl + '?order_by=period&period_from=' + startyear.isoformat() + 'T00:00:00Z&period_to=' + yesterday.isoformat() + 'T23:59:59Z&page_size=' + str(expectedcount), auth=(auth,''))
+        self.set_state('sensor.octopus_yearly_usage', state = round(yearlyusage,2))
+        self.set_state('sensor.octopus_yearly_cost', state = round(yearlycost/100,2))
+        self.set_state('sensor.octopus_monthly_usage', state = round(monthlyusage,2))
+        self.set_state('sensor.octopus_monthly_cost', state = round(monthlycost/100,2))
 
-rcost = requests.get(url=costurl + '?period_from=' + startyear.isoformat() + 'T00:00:00Z&period_to=' + yesterday.isoformat() + 'T23:59:59Z')
+    def calculate_count(self, start):
+        numberdays = self.yesterday-start
+        numberdays = numberdays.days
+        self.expectedcount = ((numberdays+1)*48)-1
 
-jconsumption = json.loads(rconsumption.text)
-jcost = json.loads(rcost.text)
+    def calculate_cost_and_usage(self, start):
+        self.calculate_count(start=start)
+        rconsumption = requests.get(url=self.consumptionurl + '?order_by=period&period_from=' + start.isoformat() + 'T00:00:00Z&period_to=' + self.yesterday.isoformat() + 'T23:59:59Z&page_size=' + str(self.expectedcount), auth=(self.auth,''))
 
-jconsumption[u'count']
+        rcost = requests.get(url=self.costurl + '?period_from=' + start.isoformat() + 'T00:00:00Z&period_to=' + self.yesterday.isoformat() + 'T23:59:59Z')
 
-expectedcount= ((numberdays+1)*48)-1
-expectedcount
+        jconsumption = json.loads(rconsumption.text)
+        jcost = json.loads(rcost.text)
 
-usage = 0
-price = 0
-cost = []
+        jconsumption[u'count']
 
-results = jconsumption[u'results']
+        usage = 0
+        price = 0
+        cost = []
 
-while jcost[u'next']:
-    cost.extend(jcost[u'results'])
-    rcost = requests.get(url=jcost[u'next'])
-    jcost = json.loads(rcost.text)
+        results = jconsumption[u'results']
 
-cost.extend(jcost[u'results'])
-cost.reverse()
+        while jcost[u'next']:
+            cost.extend(jcost[u'results'])
+            rcost = requests.get(url=jcost[u'next'])
+            jcost = json.loads(rcost.text)
 
-for period in results:
-    curridx = results.index(period)
-    usage = usage + (results[curridx][u'consumption'])
-    if (results[curridx][u'interval_start']) != (cost[curridx][u'valid_from']):
-        print('Unmatched consumption {} / cost {}'.format(results[curridx][u'interval_start'],cost[curridx][u'valid_from']))
-    price = price + ((cost[curridx][u'value_inc_vat'])*(results[curridx][u'consumption']))
+        cost.extend(jcost[u'results'])
+        cost.reverse()
 
-print('Total usage: {} kWh'.format(usage))
-print('Total cost: {} p'.format(price))
+        for period in results:
+            curridx = results.index(period)
+            usage = usage + (results[curridx][u'consumption'])
+            if (results[curridx][u'interval_start']) != (cost[curridx][u'valid_from']):
+                print('Unmatched consumption {} / cost {}'.format(results[curridx][u'interval_start'],cost[curridx][u'valid_from']))
+            price = price + ((cost[curridx][u'value_inc_vat'])*(results[curridx][u'consumption']))
+        return usage, price
