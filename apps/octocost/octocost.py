fd0c13078d10e34b5b6874bab7d6b64db13841a6
Guy Badman <61918526+badguy99@users.noreply.github.com>
2020-04-27 03:23:50 +0100

    Tidy up logging and pep8 changes

--- apps/octocost/octocost.py
+++ apps/octocost/octocost.py
@@ -47,18 +47,18 @@ class OctoCost(hass.Hass):
 
         for hour in [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]:
             self.run_daily(self.cost_and_usage_callback,
-                            datetime.time(hour, 5, 0),
-                            use=consumptionurl,
-                            cost=costurl,
-                            date=elecstartdate)
+                           datetime.time(hour, 5, 0),
+                           use=consumptionurl,
+                           cost=costurl,
+                           date=elecstartdate)
 
             if gas:
                 self.run_daily(self.cost_and_usage_callback,
-                                datetime.time(hour, 7, 0),
-                                use=gasconsumptionurl,
-                                cost=gascosturl,
-                                date=gasstartdate,
-                                gas=True)
+                               datetime.time(hour, 7, 0),
+                               use=gasconsumptionurl,
+                               cost=gascosturl,
+                               date=gasstartdate,
+                               gas=True)
 
     def find_region(self, mpan):
         url = 'https://api.octopus.energy/v1/electricity-meter-points/' + \
@@ -86,13 +86,13 @@ class OctoCost(hass.Hass):
 
         monthlyusage, monthlycost = self.calculate_cost_and_usage(
             start=startmonth)
-        print('Total monthly usage: {}'.format(monthlyusage))
-        print('Total monthly cost: {} p'.format(monthlycost))
+        self.log('Total monthly usage: {}'.format(monthlyusage), level='INFO')
+        self.log('Total monthly cost: {} p'.format(monthlycost), level='INFO')
 
         yearlyusage, yearlycost = self.calculate_cost_and_usage(
             start=startyear)
-        print('Total yearly usage: {}'.format(yearlyusage))
-        print('Total yearly cost: {} p'.format(yearlycost))
+        self.log('Total yearly usage: {}'.format(yearlyusage), level='INFO')
+        self.log('Total yearly cost: {} p'.format(yearlycost), level='INFO')
 
         if not self.gas:
             self.set_state('sensor.octopus_yearly_usage',
@@ -149,6 +149,13 @@ class OctoCost(hass.Hass):
                              start.isoformat() + 'T00:00:00Z&period_to=' +
                              self.yesterday.isoformat() + 'T23:59:59Z')
 
+        if rconsumption.status_code != 200:
+            self.log('Error getting consumption data: {}'.format(
+                     rconsumption.text), level='ERROR')
+        if rcost.status_code != 200:
+            self.log('Error getting cost data: {}'.format(rcost.text),
+                     level='ERROR')
+
         jconsumption = json.loads(rconsumption.text)
         jcost = json.loads(rcost.text)
 
@@ -178,17 +185,21 @@ class OctoCost(hass.Hass):
                     if consumption_date.endswith('+01:00'):
                         date_time = dateutil.parser.parse(consumption_date)
                         utc_datetime = date_time.astimezone(utc)
-                        utc_iso = utc_datetime.isoformat().replace("+00:00", "Z")
+                        utc_iso = utc_datetime.isoformat().replace(
+                            "+00:00", "Z")
                         if utc_iso == (cost[curridx][u'valid_from']):
                             (results[curridx][u'interval_start']) = utc_iso
                         else:
-                            print('UTC Unmatched consumption {}'.format(
+                            self.log('UTC Unmatched consumption {}'.format(
                                 results[curridx][u'interval_start']) +
-                                ' / cost {}'.format(cost[curridx][u'valid_from']))
+                                ' / cost {}'.format(
+                                    cost[curridx][u'valid_from']),
+                                level='ERROR')
                     else:
-                        print('Unmatched consumption {}'.format(
+                        self.log('Unmatched consumption {}'.format(
                             results[curridx][u'interval_start']) +
-                            ' / cost {}'.format(cost[curridx][u'valid_from']))
+                            ' / cost {}'.format(cost[curridx][u'valid_from']),
+                            level='ERROR')
                 price = price + ((cost[curridx][u'value_inc_vat']) *
                                  (results[curridx][u'consumption']))
             else:
@@ -197,7 +208,8 @@ class OctoCost(hass.Hass):
                     cost = jcost['results'][0][u'value_inc_vat']
                     price = price + cost * (results[curridx][u'consumption'])
                 else:
-                    print('Error: can only process fixed price gas')
+                    self.log('Error: can only process fixed price gas',
+                             level='ERROR')
                     price = 0
 
         return usage, price
